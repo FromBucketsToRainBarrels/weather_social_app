dce2ccb7d8c61c107dd2c37e3c6d0b67ea721f97
tanzeel <tanzeelrana@cmail.carleton.ca>
2016-11-11 05:40:20 -0500

    removing old directory structure

--- www/js/controllers/StationDetailCtrl.js
+++
@@ -1,4 +0,0 @@
-app.controller('StationDetailCtrl', function($scope, $stateParams, Stations, $timeout, ionicMaterialInk, ionicMaterialMotion) {
-	$scope.station = Stations.get($stateParams.stationId);
-	console.log($scope.station);
-});
