d0fdf4b383b82d73857e73d37a999da7ef9f23f4
tanzeelrana <tanzeelrana@live.com>
2017-02-11 01:09:24 -0500

    added scroll up down auto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,31 @@ import { RegisterPage } from '../pages/register/register';
 
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp
+      /*
+       * MODIFY BOOTSTRAP CODE BELOW
+       * Adds a config object that disables scrollAssist and autoFocusAssist for iOS only
+       * https://github.com/driftyco/ionic/issues/5571
+       */
+      , {
+        platforms : {
+          android : {
+            // These options are available in ionic-angular@2.0.0-beta.2 and up.
+            scrollAssist: false,    // Valid options appear to be [true, false]
+            autoFocusAssist: false  // Valid options appear to be ['instant', 'delay', false]
+          },
+          browser : {
+            // These options are available in ionic-angular@2.0.0-beta.2 and up.
+            scrollAssist: false,    // Valid options appear to be [true, false]
+            autoFocusAssist: false  // Valid options appear to be ['instant', 'delay', false]
+          }
+          // http://ionicframework.com/docs/v2/api/config/Config/)
+        }
+      }
+      /*
+       * END MODIFY
+       */
+       )
   ],
   bootstrap: [IonicApp],
   entryComponents: [
